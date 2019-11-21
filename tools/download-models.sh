91c325fc79c373a79868c6ae9d4dd7b5c1aed595
Nicolas Agostini <n.b.agostini@gmail.com>
2019-11-21 09:51:33 -0500

    Fix script to stay on the same folder

--- tools/download-models.sh
+++ tools/download-models.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
 mkdir mobilenet-v2
-cd mobilenet-v2
+pushd mobilenet-v2
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/mobilenet_v2_1.0_224_quant.tgz
-cd
+popd
+
 mkdir inception-v4
-cd inception-v4
+pushd inception-v4
 wget https://storage.googleapis.com/download.tensorflow.org/models/inception_v4_299_quant_20181026.tgz
-cd
+popd
+
 mkdir mnas
-cd mnas
+pushd mnas
 wget https://storage.cloud.google.com/download.tensorflow.org/models/tflite/mnasnet_1.3_224_09_07_2018.tgz
-cd
+popd
+
 mkdir coco-ssd
-cd coco-ssd
+pushd coco-ssd
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
-cd
+popd
+
 mkdir multi-person-mobilenet-v1
-cd multi-person-mobilenet-v1
+pushd multi-person-mobilenet-v1
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/gpu/multi_person_mobilenet_v1_075_float.tflite
-cd
+popd
+
 mkdir deeplabv3
-cd deeplabv3
+pushd deeplabv3
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/gpu/deeplabv3_257_mv_gpu.tflite
-cd
+popd
+
 mkdir smartreply
-cd smartreply
+pushd smartreply
 wget https://storage.googleapis.com/download.tensorflow.org/models/smartreply_1.0_2017_11_01.zip
-cd
+popd
+
 mkdir mobilebert
-cd mobilebert
+pushd mobilebert
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/bert_qa/mobilebert_qa_vocab.zip
-cd
+popd
+
 mkdir textclassification
-cd textclassification
+pushd textclassification
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite/text_classification/text_classification.tflite
-cd
+popd
