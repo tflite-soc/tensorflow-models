b5a1869062cb273f3f3b858b70fdbb6cec7dd785
Nicolas Agostini <n.b.agostini@gmail.com>
2020-06-06 22:54:54 +0000

    Implement script to download all iccd2020 DNNs

--- tools/download-models-iccd2020.sh
+++ tools/download-models-iccd2020.sh
@@ -2,23 +2,33 @@
 
 echo "Saving Models to $(pwd)"
 
-mkdir mobilenet-v2
-pushd mobilenet-v2
+mkdir mobilenet-v2-quant
+pushd mobilenet-v2-quant
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/mobilenet_v2_1.0_224_quant.tgz
+tar -xf *.tgz --wildcards --no-anchored '*.tflite'
+popd
+
+mkdir mobilenet-v2-float
+pushd mobilenet-v2-float
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/mobilenet_v2_1.0_224.tgz
 tar -xf *.tgz --wildcards --no-anchored '*.tflite'
 popd
 
 
-mkdir mobilenet-v1
-pushd mobilenet-v1
+mkdir mobilenet-v1-quant
+pushd mobilenet-v1-quant
 wget https://storage.googleapis.com/download.tensorflow.org/models/mobilenet_v1_2018_08_02/mobilenet_v1_1.0_224_quant.tgz
+tar -xf *.tgz --wildcards --no-anchored '*.tflite'
+popd
+
+mkdir mobilenet-v1-float
+pushd mobilenet-v1-float
 wget https://storage.googleapis.com/download.tensorflow.org/models/mobilenet_v1_2018_02_22/mobilenet_v1_1.0_224.tgz
 tar -xf *.tgz --wildcards --no-anchored '*.tflite'
 popd
 
-mkdir resnet-v2
-pushd resnet-v2
+mkdir resnet-v2-float
+pushd resnet-v2-float
 wget https://storage.googleapis.com/download.tensorflow.org/models/tflite_11_05_08/resnet_v2_101.tgz
 tar -xf *.tgz --wildcards --no-anchored '*.tflite'
 popd
